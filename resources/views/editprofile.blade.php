655b6d3469141d88186ea668a14ccb02575b4c1c
Prospal <leonhardandrew19@gmail.com>
2022-12-11 00:21:49 +0700

    button backhome

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Edit Profile Content</title>
+    <link rel="stylesheet" href="../css/editprofile.css">
 </head>
 <body>
+    <div>
+        <button type="submit" class="head">
+            <a href="menuuser">Back To Home</a>
+        </button>
+    </div>
+    <br>
     {{-- make table for show picture in database --}}
     <table>
         <tr>
